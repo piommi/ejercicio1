ce2b5adae086e7150b06b1e068508d3a74fb7371
paulaiommi <paulaiommi@hotmail.com>
2023-10-02 20:51:46 -0300

    primer commit

---
+++ ejercicio.js
@@ -0,0 +1,59 @@
+/* for (let i=0; i = 6; i++){
+    console.log(`Lista de Ingredientes: ${i}`)
+} 
+
+let ingredientes = prompt ("Ingrese un ingrediente")
+
+for (let i=1; i<=6; i++) {
+    console.log 
+}
+*/
+
+/* let comidas = prompt ("Que vas a cocinar")
+
+switch (comidas) {
+    case "sandwich":
+        console.log (`Para hacerte un ${comidas} necesitas: pan, jamon y queso`)
+        break
+    case "tarta":
+        console.log (`Para hacerte una ${comidas} necesitas: pollo, crema y masa de tarta `)
+        break
+    case "omelette":
+        console.log (`Para hacerte un ${comidas} necesitas: huevos, condimentos y queso`)
+        break
+    case "pancho":
+        console.log (`Para hacerte un ${comidas} necesitas: salchicha, pan, ketchup`)
+        break
+        default:
+        console.log (`Para hacerte un ${comidas} necesitas:`)
+        break
+}
+*/
+
+let sandwich= prompt (`Bienvenido/a a la sandwicheria: Elija el sandwich que desea comer: 
+1: Sandwich común
+2: Sandwich especial
+3: Sandwich completo`)
+
+switch (sandwich){
+    case "1":
+        console.log ("Usted eligió nuestro sandwich común, este incluye: pan, jamón y queso.")
+        break;
+    case "2":
+        console.log ("Usted eligió nuestro sandwich especial, este incluye: pan, jamón, queso, lechuga y tomate.")
+        break;
+    case "3":
+        console.log ("Usted eligió nuestro sandwich completo, este incluye: pan, jamón, queso, lechuga, tomate y huevo.")
+        break;
+}
+
+
+/* for (let i=0; i <=4; i++){
+    console.log(`El valor del producto es de ${i}`)
+}
+*/
+
+
+
+
+
